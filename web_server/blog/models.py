712ca93afaf10c770889e15a96545d806342b822
Leeyouyoung <91044002+LeeYouYoun@users.noreply.github.com>
2022-05-10 09:56:27 +0900

    중간저장
    중간저장

--- web_server/blog/models.py
+++ web_server/blog/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+
+class Tag(models.Model):
+    name=models.CharField(max_length=50)
+    slug=models.SlugField(max_length=200,unique=True,allow_unicode=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
 class Category(models.Model):
     name=models.CharField(max_length=50,unique=True)
     slug=models.SlugField(max_length=200, unique=True,allow_unicode=True)
@@ -10,8 +20,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    class Meta:
-        verbose_name_plural='Categories'
+ #   class Meta:
+  #      verbose_name_plural='Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -26,6 +36,7 @@ class Post(models.Model):
     
     #author = models.ForeignKey(User, on_delete=models.CA)
     category=models.ForeignKey(Category,null=True,blank=True, on_delete=models.SET_NULL)
+    tags=models.ManyToManyField(Tag,null=True,blank=True)
 
 def __str__(self):
         return f'[{self.pk}]{self.title}'
