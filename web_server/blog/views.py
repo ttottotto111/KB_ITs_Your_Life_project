502f2c9d845edc6eeee379361ce517483e88bef3
Leeyouyoung <91044002+LeeYouYoun@users.noreply.github.com>
2022-05-10 09:43:22 +0900

    작업파일 합침
    협업용 파일로 이동

--- web_server/blog/views.py
+++ web_server/blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Post,Category
 
+class PostList(ListView):
+    model=Post
+    #template_name = 'blog/index.html'
+class PostDetail(DetailView):
+    model=Post
+
+#def index(request):
+ #   posts=Post.objects.all()
+ #   return render(
+ #       request,
+ #       'blog/index.html',
+ #       {
+  #          'posts': posts,
+   #     }
+   # )
+
+def get_context_data(self,**kwargs):
+    context=super(PostDetail,self).get_context_data()
+    context['categories']=Category.objects.all()
+    context['no_category_post_count']=Post.objects.filter(category=None).count()
+    return context
+
+def single_post_page(request,pk):
+    post=Post.objects.get(pk=pk)
+    
+    return render(request,
+                  'blog/single_post_page.html',
+                  {
+                      'post': post,
+                  })
+def category_page(request,slug):
+    if slug == 'no_category':
+        category='미분류'
+        post_list=Post.objects.filter(category=None)
+    else:
+        category=Category.objects.get(slug=slug)
+        post_list=Post.oobjects.filter(category=category)
+    
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list':Post.objects.filter(category=category),
+            'categories':Category.objects.all(),
+            'no_category_post_count':Post.objects.filter(category=None).count(),
+            'category':category,
+        }
+    )
 # Create your views here.
