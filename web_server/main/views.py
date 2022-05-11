19230850a9e1e4a5cf915d499f092d42a5050876
ttottotto111 <74037026+ttottotto111@users.noreply.github.com>
2022-05-11 17:31:06 +0900

    드롭다운 수정, db업로드 삭제 추가
    ajax를 통한 차량 검색 추가 예정

--- web_server/main/views.py
+++ web_server/main/views.py
@@ -1,25 +1,27 @@
 import json
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from blog.models import Post
+from .models import *
 
 # Create your views here.
 def main(request):
     posts = Post.objects.all().order_by('-pk')
+    maker = ['현대', '기아']
+    h = ['그랜저', '소나타', '투싼']
+    g = ['k5', 'k7', 'ev6']
     
     return render(request,"main/main.html",
                   {
-                      'posts':posts
+                      'posts':posts,
+                      'maker':maker,
+                      'h': h,
+                      'g':g
                   })
 
-def car_maker(request):
-    if request.method == 'POST':
-        #테스트용 포스트파일
-        posts = Post.objects.all().order_by('-pk')
-        
-        data = request.POST
-        maker = data.get('maker')
-        if(maker=="현대"):
-            return render(request, "blog/post_list.html",
-                        {
-                            'maker':posts
-                        })
+def car_maker(request, maker):
+    if(maker == "hyundae"):
+        data = {
+            '차종': ['그랜저', '소나타', '투싼']
+        }
+        return JsonResponse(data)
